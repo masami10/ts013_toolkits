9d17894fdeb9a02f5c5d9dc91c714401e49c8a84
Cosine <cooosine@outlook.com>
2021-07-23 07:11:39 +0800

    [FIX](tcp_client): 修复关闭连接

--- transport/tcp_client.py
+++ transport/tcp_client.py
@@ -2,6 +2,7 @@ import socket
 import threading
 from typing import Optional, Callable
 import time
+import io
 import platform
 import os
 from loguru import logger
@@ -25,24 +26,26 @@ class TcpClient(object):
         self._server_addr = (ip, int(port))
         self._client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    def connect(self):
+    def connect(self, on_start, notify):
         if not self._server_addr:
             raise Exception('Server Address Is Empty')
         if not self._client:
             raise Exception('Client Is Empty')
         if self.connected:
             raise Exception('连接已存在')
-        # self._client.settimeout(2.0)  # 设置2秒timeout
         if ENV_KEEPALIVE_ENABLE and platform.system() == 'Windows':
             self._client.ioctl(socket.SIO_KEEPALIVE_VALS, (1, 5000, 1000))  # 正常连接时候5s一次，没有的时候1秒一次，最多5次
         self._client.connect(self._server_addr)
         self.connected = True
-        return True
+        notify.info("TCP 客户端线程打开")
+        on_start(True)
+        self.started = True
+        self.run()
 
     def disconnect(self):
         if not self._client:
             return
-        self._client.close()
+        self._client.shutdown(socket.SHUT_RDWR)
         self.connected = False
 
     def set_handler(self, handler: Callable[[str], None]):
@@ -50,48 +53,40 @@ class TcpClient(object):
             self.handler = handler
 
     def run(self):
-        f = self._client.makefile('r', encoding='utf-8', newline=self._newline)
-        while self.started:
-            if not self.connected:
-                logger.info("等待连接")
-                time.sleep(0.5)
-                continue
-            line = f.readline()
-            if not line:
-                logger.info("客户端断开，请重新连接")
-                self.do_stop()
-                return
-            if isinstance(line, bytes):
-                line = line.decode(encoding='utf-8')
-            line = line.split(self._newline)[0]
-            logger.debug(f"接收到数据{line}")
-            if self.handler:
-                self.handler(line)
+        with self._client.makefile('r', encoding='utf-8', newline=self._newline) as f:
+            while self.started:
+                if not self.connected:
+                    logger.info("等待连接")
+                    time.sleep(0.5)
+                    continue
+                try:
+                    line = f.readline()
+                except Exception as e:
+                    # logger.error(e)
+                    continue
+                if not line:
+                    logger.info("客户端断开，请重新连接")
+                    self.do_stop()
+                    return
+                if isinstance(line, bytes):
+                    line = line.decode(encoding='utf-8')
+                line = line.split(self._newline)[0]
+                logger.debug(f"接收到数据{line}")
+                if self.handler:
+                    self.handler(line)
         f.close()
-
-    # def do_connect(self):
-    #     result = run_and_get_result(self.connect())
-    #     return result
 
     def _do_start(self, on_start, notify):
         notify.info("TCP 客户端启动中...")
-        self.connect()
-        self.started = True
-        self.thread = threading.Thread(target=self.run, name=self._name)
+        self.thread = threading.Thread(target=self.connect, args=(on_start, notify,), name=self._name)
         self.thread.setDaemon(True)
         self.thread.start()
-        notify.info("TCP 客户端线程打开")
-        on_start(True)
 
     def start(self, on_start, notify):
-        # if self.start_task:
-        #     self.start_task.close()
         self.on_client_status = on_start
         self._do_start(on_start, notify)
 
     def do_stop(self):
-        if self.start_task:
-            self.start_task.close()
         self.started = False
         self.disconnect()
         if self.on_client_status:
