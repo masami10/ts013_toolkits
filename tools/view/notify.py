7e272ae97e8a9635a3dea5e1adc7bdaeb2da7085
kevin0120 <jingwei1224@163.com>
2021-09-01 13:58:26 +0800

    [FIX](notify): 使用signal-emit的方式渲染日志

--- tools/view/notify.py
+++ tools/view/notify.py
@@ -1,27 +1,33 @@
 # -*- coding: utf-8 -*-
-from PyQt5.QtWidgets import QTextBrowser
+from PyQt5.QtWidgets import QTextBrowser, QWidget
 from typing import List
 from transport.constants import now
-from tools.view.mixin import ToolKitMixin
 from loguru import logger
+from PyQt5 import QtCore
 
 
-class ToolkitNotify:
+class ToolkitNotify(QWidget):
     qt_instances: List[QTextBrowser]
+    notify_append_signal = QtCore.pyqtSignal(str)
 
     def __init__(self, instances: List[QTextBrowser]):
+        QWidget.__init__(self)
         self.qt_instances = []
         for instance in instances:
             self.qt_instances.append(instance)
+        self.notify_append_signal.connect(self.handle_notify_append)
+
+    def handle_notify_append(self, content: str):
+        for instance in self.qt_instances:
+            instance.append(content)
+            instance.verticalScrollBar().setValue(instance.verticalScrollBar().maximum())
 
     def _notify(self, color: str, content: str):
         if not self.qt_instances or len(self.qt_instances) == 0:
             return
         data = f'时间:{now()}, 内容: {content}'
         ss = "<span style=\" font-size:10pt; font-weight:600; color:#{};\" > {} </span>".format(color, data)
-        for instance in self.qt_instances:
-            instance.append(ss)
-            instance.verticalScrollBar().setValue(instance.verticalScrollBar().maximum())
+        self.notify_append_signal.emit(ss)
 
     def error(self, content: str):
         color = 'e33371'
