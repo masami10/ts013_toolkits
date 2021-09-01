9eeb4e9f1c1cf2928a431363f5b90bca6de80c9e
kevin0120 <jingwei1224@163.com>
2021-09-01 11:15:51 +0800

    人员信息与连接信息报错问题

--- tools/controller/ConnectionController.py
+++ tools/controller/ConnectionController.py
@@ -8,8 +8,9 @@ class ConnectionController:
         self.notify = self.window.notify_box
         self._config = config
         self.window.config_input_group.inputChanged.connect(self.on_config_input)
-        self.window.person_config_group.inputChanged.connect(self.on_config_input)
+        self.window.person_config_group.inputChanged.connect(self.on_config_input1)
         self.render()
+        self.render1()
 
     @property
     def contents(self):
@@ -18,10 +19,6 @@ class ConnectionController:
             'momUrl': self._config.get_config('momUrl'),
             'operationUrl': self._config.get_config('operationUrl'),
             'workCenter': self._config.rawWorkCenters,
-            'originPersonCode': self._config.get_config('originPersonCode'),
-            'originPersonName': self._config.get_config('originPersonName'),
-            'recheckPersonCode': self._config.get_config('recheckPersonCode'),
-            'recheckPersonName': self._config.get_config('recheckPersonName'),
         }
 
     def on_config_input(self, key, value):
@@ -33,4 +30,23 @@ class ConnectionController:
 
     def render(self):
         self.window.config_input_group.set_texts(self.contents)
-        self.window.person_config_group.set_texts(self.contents)
+
+
+    @property
+    def contents1(self):
+        return {
+            'originPersonCode': self._config.get_config('originPersonCode'),
+            'originPersonName': self._config.get_config('originPersonName'),
+            'recheckPersonCode': self._config.get_config('recheckPersonCode'),
+            'recheckPersonName': self._config.get_config('recheckPersonName'),
+        }
+
+    def on_config_input1(self, key, value):
+        if self.contents.get(key, None) == value:
+            return
+        self.notify.info('配置输入：{}，{}'.format(key, value))
+        self._config.set_config(key, value)
+        self.render1()
+
+    def render1(self):
+        self.window.person_config_group.set_texts(self.contents1)
